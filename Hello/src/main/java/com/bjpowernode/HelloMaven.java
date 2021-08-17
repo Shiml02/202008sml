e6c42a5f812eef45ef609e7d33f0da3b03abc659
sml <1456939864@qq.com>
2021-08-17 11:54:03 +0800

    1

---
+++ Hello/src/main/java/com/bjpowernode/HelloMaven.java
@@ -0,0 +1,13 @@
+package com.bjpowernode;
+public class HelloMaven {
+
+  public int add(int n1,int n2){
+    return n1+n2;
+  }
+
+  public static void main(String args[]){
+      HelloMaven hello = new HelloMaven();
+      int res = hello.add(10,20);
+		System.out.println("10+20="+res);
+  }
+}
